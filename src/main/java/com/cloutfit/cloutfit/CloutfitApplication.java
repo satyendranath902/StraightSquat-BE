3b5b01b57314ea25d42c41c41a9b6b5ea733e638
satyendranath902 <satyendranath902@gmail.com>
2023-06-15 00:21:55 +0530

    cloutfit spring initializer

---
+++ src/main/java/com/cloutfit/cloutfit/CloutfitApplication.java
@@ -0,0 +1,13 @@
+package com.cloutfit.cloutfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloutfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloutfitApplication.class, args);
+	}
+
+}
